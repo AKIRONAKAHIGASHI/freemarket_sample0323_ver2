89a2363ea3ef07da4c6589b471038bf1c1c6a340
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-04-27 21:31:23 +0900

    Exhibit.view

--- app/controllers/purchase_confirmations_controller.rb
+++ app/controllers/purchase_confirmations_controller.rb
@@ -1,4 +1,5 @@
-class PurchaseConfirmationsController < ApplicationController
-  def index
-  end
-end
+# これから編集予定
+# class PurchaseConfirmationsController < ApplicationController
+#   def index
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
   resources :items
-  # resources :products
   resources :purchase_confirmations
 end
